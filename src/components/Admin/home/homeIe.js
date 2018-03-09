1bcba09e2cc7f19a29745d9aa556af3d7fabae52
hunterlegend <850899364@qq.com>
2018-03-09 10:58:03 +0800

    bizcharts不支持IE9处理

---
+++ src/components/Admin/home/homeIe.js
@@ -0,0 +1,298 @@
+import React, {Component} from 'react'
+import Utility from 'UTIL/utility'
+
+var echarts = require('echarts')
+
+class HomeIe extends Component {
+    constructor(props) {
+		super(props)
+        this.state = {
+            aliMoney: 0,
+            wxMoney: 0
+        }
+	}
+    
+
+    componentWillMount() {
+     
+
+    }
+
+    componentDidMount() {
+        this.generateMachineSituation()
+        this.generateIncome()
+        // this.generateDynamicData()
+        // this.generateTotalMoney()
+    }
+
+    generateTotalMoney = () => {
+         this.props.fetchTotalMoney().then(msg => {
+            if (this.props.totalMoney && this.props.totalMoney.data && this.props.totalMoney.data instanceof Array) {
+                let aliM = 0
+                let wxM = 0
+                
+                this.props.totalMoney.data.map((item, index) => {
+                    
+                    if (item.AliAccount) {
+                        aliM = aliM + parseFloat(item.AliAccount)
+                    }
+                    if (item.WxAccount) {
+                        wxM = wxM + item.WxAccount
+                    }
+                    
+                })
+
+                this.setState({aliMoney: aliM, wxMoney: wxM})
+            }
+        })
+    }
+
+    generateMachineSituation = () => {
+       // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('machineSituation'))
+       
+        this.props.fetchTotalMachineCount().then(msg => {
+            if (this.props.totalMoney.totalMachine) {
+                let countData = JSON.parse(this.props.totalMoney.totalMachine)
+                
+                 // 绘制图表
+                myChart.setOption({
+                    title: {
+                        text: '机器情况',
+                        x: 'center',
+                        subtext: parseInt(countData[0].VALS, 0) + parseInt(countData[1].VALS, 0) + parseInt(countData[2].VALS, 0)
+                    },
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: '{a} <br/>{b} : {c} ({d}%)'
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        left: 'left',
+                        data: ['未启用', '离线', '在线']
+                    },
+                    series: [
+                        {
+                            name: '机器情况',
+                            type: 'pie',
+                            radius: '55%',
+                            center: ['50%', '60%'],
+                            data: [
+                                {
+                                    value: parseInt(countData[0].VALS, 0), 
+                                    name: '未启用',
+                                    itemStyle: {
+                                        normal: {
+                                            // 设置扇形的颜色
+                                            color: '#E6E6E2',
+                                            shadowBlur: 10,
+                                            shadowColor: 'rgba(0, 0, 0, 0.1)'
+                                        }
+                                    }
+                                },
+                                {value: parseInt(countData[1].VALS, 0), name: '离线'},
+                                {
+                                    value: parseInt(countData[2].VALS, 0), 
+                                    name: '在线',
+                                    itemStyle: {
+                                        normal: {
+                                            // 设置扇形的颜色
+                                            color: '#54bd14',
+                                            shadowBlur: 10,
+                                            shadowColor: 'rgba(0, 0, 0, 0.1)'
+                                        }
+                                    }
+                                }
+                                 
+                            ]
+                        }
+                    ]
+                })
+            }
+        })
+       
+    }
+
+    generateIncome = () => {
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('dynamicData'))
+        let nowWeek = Utility.getCurrentWeekDate()
+         this.props.fetchAmountByMachine({salesDateStart: Utility.dateFormaterString(nowWeek[0]), salesDateEnd: Utility.dateFormaterString(nowWeek[1])}).then(msg => {
+                if (this.props.totalMoney && this.props.totalMoney.amountByMachine) {
+                    let finalData = JSON.parse(this.props.totalMoney.amountByMachine)
+                    let xData = []
+                    let yData = []
+                    $.each(finalData, (index, item) => {
+                        xData.push(item.MACHINE_ID)
+                        yData.push(item.TOTAL)
+                    })
+                    // 绘制图表
+                    myChart.setOption({
+                        title: {
+                            text: '当前周销售额',
+                            x: 'center',
+                            subtext: ''
+                        },
+                        color: ['#3398DB'],
+                        tooltip: {
+                            trigger: 'axis',
+                            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                            }
+                        },
+                        grid: {
+                            left: '3%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        xAxis: [
+                            {
+                                type: 'category',
+                                data: xData,
+                                axisTick: {
+                                    alignWithLabel: true
+                                }
+                            }
+                        ],
+                        yAxis: [
+                            {
+                                type: 'value'
+                            }
+                        ],
+                        series: [
+                            {
+                                name: '销售额',
+                                type: 'bar',
+                                barWidth: '40%',
+                                data: yData
+                            }
+                        ]
+                    })
+                }
+         })
+        
+    }
+
+
+    generateDynamicData = () => {
+         let myChart = echarts.init(document.getElementById('dynamicData'))
+        function randomData() {
+            now = new Date(+now + oneDay)
+            value = value + Math.random() * 21 - 10
+            return {
+                name: now.toString(),
+                value: [
+                    [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+                    Math.round(value)
+                ]
+            }
+        }
+
+        var data = []
+        var now = +new Date(1997, 9, 3)
+        var oneDay = 24 * 3600 * 1000
+        var value = Math.random() * 1000
+        for (var i = 0; i < 1000; i++) {
+            data.push(randomData())
+        }
+
+        let option = {
+            title: {
+                text: '动态数据',
+                x: 'center'
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter: function (params) {
+                    params = params[0]
+                    var date = new Date(params.name)
+                    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1]
+                },
+                axisPointer: {
+                    animation: false
+                }
+            },
+            xAxis: {
+                type: 'time',
+                splitLine: {
+                    show: false
+                }
+            },
+            yAxis: {
+                type: 'value',
+                boundaryGap: [0, '100%'],
+                splitLine: {
+                    show: false
+                }
+            },
+            series: [{
+                name: '模拟数据',
+                type: 'line',
+                showSymbol: false,
+                hoverAnimation: false,
+                data: data
+            }]
+        }
+
+        myChart.setOption(option)
+
+        setInterval(function () {
+
+            for (var i = 0; i < 5; i++) {
+                data.shift()
+                data.push(randomData())
+            }
+
+            myChart.setOption({
+                series: [{
+                    data: data
+                }]
+            })
+        }, 1000)
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    
+
+    shouldComponentUpdate(nextProps, nextState) { // 组件判断是否重新渲染时调用
+        return true
+    }
+
+    render() {
+        return (
+            <div className="homeIeContainer">
+               <div>
+                  <div style={{borderRight: '5px solid #eee' }}>
+                     <div id="machineSituation" className="everyChartsHeight"></div>
+                  </div>
+                  <div style={{borderLeft: '5px solid #eee' }}>
+                       <h4 style={{fontWeight: 'bold'}}></h4>
+                      <div id="incomeSituation" className="everyChartsHeight">
+                         
+                         
+                      </div>
+                  </div>
+               </div>
+               <div>
+                  <div className="everyChartsHeight" id = "dynamicData" style={{marginTop: '10px', borderTop: '10px solid #eee'}}>
+                      
+                  </div>
+               </div>
+           </div>
+        )
+    }
+}
+
+export default HomeIe
---
+++ src/components/Admin/home/homeOther.js
@@ -0,0 +1,284 @@
+import React, {Component} from 'react'
+// import Utility from 'UTIL/utility'
+import { Chart, Geom, Tooltip, Coord, Label, Axis } from 'bizcharts'
+import {Icon} from 'antd'
+import DataSet from '@antv/data-set'
+import SalesMoney from 'COMPONENT/Admin/home/salesMoney'
+
+const { DataView } = DataSet
+
+const dv = new DataView()
+const cols = {
+    percent: {
+      formatter: val => {
+        val = (val * 100) + '%'
+        return val
+      }
+    }
+  }
+
+
+
+// var echarts = require('echarts')
+
+class HomeOther extends Component {
+    constructor(props) {
+		super(props)
+        this.state = {
+            machineSituationData: [],
+            totalMachine: 0,
+            payTotalNumbers: 0,
+            successReate: '100%',
+            payEveryData: [{ year: '2018/02/28', sales: 38 },
+            { year: '2018/03/01', sales: 52 },
+            { year: '2018/03/02', sales: 61 },
+            { year: '2018/03/03', sales: 145 },
+            { year: '2018/03/04', sales: 48 },
+            { year: '2018/03/05', sales: 38 },
+            { year: '2018/03/06', sales: 38 },
+            { year: '2018/03/07', sales: 38 }],
+            threeMonthMoney: [{Data: 1}, {Data: 1}, {Data: 1}]
+        }
+	}
+    
+
+    componentWillMount() {
+       
+
+    }
+
+    componentDidMount() {
+        $('.childrenContainer').css('background-color', '#dcdedd')
+        this.generateMachineSituation()
+        this. generateTotalPayNumbers()
+        this.generateGroupMoney()
+        // this.generateIncome()
+        // this.generateDynamicData()
+        // this.generateTotalMoney()
+    }
+
+    generateMachineSituation = () => {
+       
+        this.props.fetchTotalMachineCount().then(msg => {
+            if (this.props.totalMoney.totalMachine) {
+                let countData = JSON.parse(this.props.totalMoney.totalMachine)
+                this.setState({machineSituationData: [
+                    { item: '在线', count: parseInt(countData[2].VALS, 0) },
+                    { item: '离线', count: parseInt(countData[1].VALS, 0) },
+                    { item: '未启用', count: parseInt(countData[0].VALS, 0) }
+                  ], totalMachine: parseInt(countData[2].VALS, 0) + parseInt(countData[1].VALS, 0) + parseInt(countData[0].VALS, 0)})
+            }
+        })
+       
+    }
+
+    generateTotalPayNumbers = () => {
+        this.props.fetchPayNumbers().then(msg => {
+            let numbers = this.props.totalMoney.payNumbers
+           if (numbers) {
+              let successNum = 0
+              let totalNum = 0
+              for (var i = 0; i < numbers.length; i++) {
+                 if (numbers[i].Name == 2) {
+                    successNum = numbers[i].Data
+                 }
+                 totalNum = totalNum + parseInt(numbers[i].Data, 0)
+
+              }
+
+              this.setState({payTotalNumbers: totalNum, successReate: ((successNum / totalNum) * 100).toFixed(2) + '%'})
+           }
+        })
+    }
+
+    generateGroupMoney = () => {
+        let date = new Date()
+        let year = date.getFullYear()
+        let month = date.getMonth() + 1
+        let day = date.getDate()
+        let hour = date.getHours()
+        let minute = date.getMinutes()
+        let second = date.getSeconds()
+
+        let endDate = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second
+        let endMonth = month - 2
+        let endYear = year
+        if (endMonth < 1) {
+            endMonth = 12 + endMonth
+            endYear = endYear - 1
+        }
+        let startDate = endYear + '/' + endMonth + '/' + day + ' 00:00:00'
+        this.props.fetchGroupMoney({salesDateStart: startDate, salesDateEnd: endDate, type: 'month'}).then(msg => {
+            this.setState({threeMonthMoney: this.props.totalMoney.groupMoney})
+        })
+    }
+
+    generateIncome = () => {
+        /*
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('dynamicData'))
+        let nowWeek = Utility.getCurrentWeekDate()
+         this.props.fetchAmountByMachine({salesDateStart: Utility.dateFormaterString(nowWeek[0]), salesDateEnd: Utility.dateFormaterString(nowWeek[1])}).then(msg => {
+                if (this.props.totalMoney && this.props.totalMoney.amountByMachine) {
+                    let finalData = JSON.parse(this.props.totalMoney.amountByMachine)
+                    let xData = []
+                    let yData = []
+                    $.each(finalData, (index, item) => {
+                        xData.push(item.MACHINE_ID)
+                        yData.push(item.TOTAL)
+                    })
+                    // 绘制图表
+                    myChart.setOption({
+                        title: {
+                            text: '当前周销售额',
+                            x: 'center',
+                            subtext: ''
+                        },
+                        color: ['#3398DB'],
+                        tooltip: {
+                            trigger: 'axis',
+                            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                            }
+                        },
+                        grid: {
+                            left: '3%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        xAxis: [
+                            {
+                                type: 'category',
+                                data: xData,
+                                axisTick: {
+                                    alignWithLabel: true
+                                }
+                            }
+                        ],
+                        yAxis: [
+                            {
+                                type: 'value'
+                            }
+                        ],
+                        series: [
+                            {
+                                name: '销售额',
+                                type: 'bar',
+                                barWidth: '40%',
+                                data: yData
+                            }
+                        ]
+                    })
+                }
+         })
+         */
+        
+    }
+
+
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    
+
+    shouldComponentUpdate(nextProps, nextState) { // 组件判断是否重新渲染时调用
+        return true
+    }
+
+    showMachineSituation = (arr) => {
+        dv.source(arr).transform({
+            type: 'percent',
+            field: 'count',
+            dimension: 'item',
+            as: 'percent'
+        })
+    }
+
+    render() {
+
+        this.showMachineSituation(this.state.machineSituationData)
+
+        let monthCompare = ((this.state.threeMonthMoney[1].Data / this.state.threeMonthMoney[0].Data) - 1) * 100
+      
+        return (
+            <div>
+            <div className="homeContainer">
+               <div>
+                <div className="homeTitle">机器情况</div>
+               <Chart height={200} data={dv} scale={cols} padding={[ 8 ]} forceFit>
+               <Coord type='theta' radius={0.75} />
+               <Axis name="percent" />
+               <Tooltip 
+                 showTitle={false} 
+                 itemTpl='<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
+                 />
+               <Geom
+                 type="intervalStack"
+                 position="percent"
+                 color={['item', [ '#54bd14', '#ea0e3c', '#71E3E3']]}
+                 tooltip={['item*percent', (item, percent) => {
+                   percent = (percent * 100).toFixed(2) + '%'
+                   return {
+                     name: item,
+                     value: percent
+                   }
+                 }]}
+                 style={{lineWidth: 1, stroke: '#fff'}}
+                 >
+                 <Label content={['item*count', (item, count) => {
+                   // percent = percent * 100 + '%'
+                   return item + ':' + count
+                 }]} />
+               </Geom>
+             </Chart>
+             <div className="homeFooter">
+                 <span>总机器数量</span><span>{this.state.totalMachine}</span>
+             </div>
+               </div>
+               <div>
+                   <div className="homeSecondTitle">
+                       总支付笔数
+                   </div>
+                   <div className="totalNumberSize">{this.state.payTotalNumbers}</div>
+                   <Chart padding={[ 0, 0, 0, 0]} height={178} data={this.state.payEveryData} forceFit>
+                        <Tooltip crosshairs={{type: 'y'}}/>
+                        <Geom type="interval" position="year*sales" />
+                    </Chart>
+                    <div className="homeFooter">
+                        <span>成功转化率</span><span>{this.state.successReate}</span>
+                    </div>
+               </div>
+               <div>
+                    <div className="homeTitle">
+                       近三月销售额
+                   </div>
+                   {
+                       this.state.threeMonthMoney.map((item, index) => {
+                           return (<div className="totalNumberSize" style={{fontSize: '18px', marginBottom: '5px'}}>{item.Name + '年' + item.Name1 + '月 : '}¥{item.Data}</div>)
+                       })
+                   }
+                  
+                    <div className="homeFooter" style = {{marginTop: '120px'}}>
+                        <span>月环比</span><span>{monthCompare.toFixed(2) + '%'} <Icon type={monthCompare < 0 ? 'caret-down' : 'caret-up'} style={{color: monthCompare < 0 ? '#ea0e3c' : '#54bd14'}} /></span>
+                    </div>
+               </div>
+               <div></div>
+           </div>
+           <SalesMoney />
+           </div>
+        )
+    }
+}
+
+export default HomeOther
