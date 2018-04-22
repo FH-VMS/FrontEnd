e380633dbfcd40a345e0e77cb0a995d826e15197
DESKTOP-E7P98AS\landry <850899364@qq.com>
2018-04-22 21:06:22 +0800

    wechat mall

---
+++ src/components/wechat/mall/everyProduct.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+class EveryProduct extends Component {
+	constructor(props) {
+        super(props)
+	}
+
+  componentWillMount() {
+    
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+      return (
+        <div className="everyProduct">
+            购物车
+        </div>
+        )
+  }
+  
+}
+
+export default EveryProduct
+
--- src/views/wechat/mall.js
+++ src/views/wechat/mall.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Carousel, Tabs, Badge} from 'antd-mobile'
+import EveryProduct from 'COMPONENT/wechat/mall/everyProduct'
 
 class Mall extends Component {
 	constructor(props) {
@@ -59,13 +60,16 @@ class Mall extends Component {
               onChange={(tab, index) => { console.log('onChange', index, tab) }}
               onTabClick={(tab, index) => { console.log('onTabClick', index, tab) }}
             >
-              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                Content of first tab
+              <div className="tabItem">
+                <EveryProduct />
+                <EveryProduct />
+                <EveryProduct />
+                <EveryProduct />
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+              <div className="tabItem">
                 Content of second tab
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+              <div className="tabItem">
                 Content of third tab
               </div>
             </Tabs>
