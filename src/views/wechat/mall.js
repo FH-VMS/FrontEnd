defe4a6b5ef6c177e043c0c4229e4d7a9d10d6ee
hunterlegend <850899364@qq.com>
2018-04-28 10:05:29 +0800

    根据类型取商品

--- src/views/wechat/mall.js
+++ src/views/wechat/mall.js
@@ -6,7 +6,12 @@ class Mall extends Component {
 	constructor(props) {
     super(props)
     this.state = {
-      productType: []
+      productType: [],
+      tabTitles: [],
+      tabContents: [],
+      nowTabContent: {
+
+      }
     }
 	}
 
@@ -17,7 +22,14 @@ class Mall extends Component {
     if (this.props.location.query.clientId) {
       this.props.fetchProductType({clientId: this.props.location.query.clientId}).then(msg => {
         if (this.props.wechat.productTypeData.length > 0) {
-          this.setState({productType: this.props.wechat.productTypeData})
+          let tabTitles = []
+          let tabContents = []
+          this.props.wechat.productTypeData.map((item, index) => {
+            tabTitles.push({ title: <Badge>{item.WaresTypeName}</Badge> })
+            tabContents.push(<div className="tabItem">{this.state.nowTabContent[index]}</div>)
+          })
+          this.setState({productType: this.props.wechat.productTypeData, tabTitles: tabTitles, tabContents: tabContents})
+          this.getProductById(0)
         }
       })
     }
@@ -26,23 +38,31 @@ class Mall extends Component {
 
 
   tabChange = (tab, index) => {
-
+    this.getProductById(index)
   }
 
-  getProductById = (typeIdVal) => {
-    this.props.fetchProduct({typeId: typeIdVal, clientId: this.props.location.query.clientId}).then(msg => {
-     
-    })
+  getProductById = (index) => {
+   
+    if (!this.state.nowTabContent[index]) {
+      let nowContent = []
+      this.props.fetchProduct({typeId: this.state.productType[index].WaresTypeId, clientId: this.props.location.query.clientId}).then(msg => {
+        console.log('aaaaa', this.props)
+        if (this.props.wechat.productData) {
+          this.props.wechat.productData.map((item, index) => {
+            nowContent.push(<EveryProduct data={item} />)
+          })
+          this.state.nowTabContent[index] = nowContent
+          this.setState({nowTabContent: this.state.nowTabContent})
+        }
+     })
+    }
+    
   }
 
 
   render() {
-      let tabTitles = []
-      let tabContents = []
-      this.state.productType.map((item, index) => {
-        tabTitles.push({ title: <Badge>{item.WaresTypeName}</Badge> })
-        tabContents.push(<div className="tabItem"><EveryProduct /></div>)
-      })
+     
+     
 
       return (
           <div>
@@ -80,12 +100,12 @@ class Mall extends Component {
                 }}
               />
             </Carousel>
-            <Tabs tabs={tabTitles}
+            <Tabs tabs={this.state.tabTitles}
               initialPage={0}
               onChange={this.tabChange.bind(this)}
               onTabClick={(tab, index) => { console.log('onTabClick', index, tab) }}
             >
-              {tabContents}
+              {this.state.tabContents}
             </Tabs>
           </div>
         )
